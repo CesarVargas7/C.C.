c2585ad814bf6db0571fc0c51a011a7210568406
César Antonio Vargas Rodríguez <cesar.vargas@sinergia.com.mx>
2021-10-08 23:18:05 -0500

    Prueba de ciclo

--- ContadorVector/Contador.c
+++ ContadorVector/Contador.c
@@ -5,12 +5,30 @@ void SolicitaCampo(char* formato, char* nombre, void* valor, int bufer) {
 	scanf_s(formato, valor, bufer);
 }
 
+#define MAX_NUM 10
+#define MAX_NOMBRE 15
+
 int main() {
-    int valores[3] = {0};
+    int valores[MAX_NUM] = {0};
     int dato;
+    char nombre[MAX_NOMBRE] = {0};
+    
+    printf("-----------------\n");
     printf("Hola Nene\n");
-    SolicitaCampo("%d", "dato", &dato, sizeof(dato));
-    valores[0] = dato;
+    printf("-----------------\n");
+
+    for(int c = 0; c < MAX_NUM; ++c) {
+        sprintf_s(nombre, MAX_NOMBRE, "dato %d", c+1);
+        SolicitaCampo("%d", nombre, &dato, sizeof(dato));
+        valores[c] = dato;
+    }
+
+    printf("-----------------\n");
+    printf("Datos capturados:\n");
+    
+    for(int c = 0; c < MAX_NUM; ++c) {
+        printf("%d\n", valores[c]);
+    }
 
     return 0;
 }
